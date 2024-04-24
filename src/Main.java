f7e8e7e3014020a2055a37936ddc45e91bfbe8df
linmyatoo1 <6631503066@lamduan.mfu.ac.th>
2024-04-24 22:34:46 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("1:Rent a car");
+        System.out.println("2:Return a car");
         //username input
         Scanner SignIn = new Scanner(System.in);
         System.out.println("Enter UserName");
