bc44c45236c3c08aeb247da43b1f12fec7c3d840
MHKaungPyae <156641911+MHKaungPyae@users.noreply.github.com>
2024-04-27 20:00:50 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-=======
-        RentalCar rc = new RentalCar();
-        rc.carPayment();
-
-        CarRentConfirmation rcf = new CarRentConfirmation();
-        rcf.confirm();
->>>>>>> mhkaungpyae
-=======
-//
         RentalCar car = new RentalCar();
         car.carChoice();
 >>>>>>> mhkaungpyae
